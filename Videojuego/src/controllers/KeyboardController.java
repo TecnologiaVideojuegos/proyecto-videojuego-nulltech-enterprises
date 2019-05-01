92981c55ba3f667df2fa067e3d4f3d315289e7eb
t4rmo <t4rmogoyft@gmail.com>
2019-05-01 11:28:51 +0200

    Teclado dos jugadores

--- Videojuego/src/controllers/KeyboardController.java
+++ Videojuego/src/controllers/KeyboardController.java
@@ -17,17 +17,33 @@ public class KeyboardController extends Input {
 	/*
 	 * GETTERS
 	 */
-	public int getXMovement() {
+	public int getXMovementPl1() {
 		if (isKeyDown(Input.KEY_A)) return -100;
 		if (isKeyDown(Input.KEY_D)) return 100;
 		return 0;
 	}
 	
-	public int getYMovement() {
+	public int getYMovementPl1() {
 		if (isKeyDown(Input.KEY_W)) return -100;
 		if (isKeyDown(Input.KEY_S)) return 100;
 		return 0;
 	}
+	
+	public int getXMovementPl2() {
+		if (isKeyDown(Input.KEY_LEFT)) return -100;
+		if (isKeyDown(Input.KEY_RIGHT)) return 100;
+		return 0;
+	}
+	
+	public int getYMovementPl2() {
+		if (isKeyDown(Input.KEY_UP)) return -100;
+		if (isKeyDown(Input.KEY_DOWN)) return 100;
+		return 0;
+	}
+	
+	
+	
+	
 	public String getPressedpl1(GameContainer gc)
 	{
 
@@ -43,6 +59,31 @@ public class KeyboardController extends Input {
 		if(gc.getInput().isKeyPressed(Input.KEY_D)){
 			return lastpressedp1="Right";
 			}
+		if(gc.getInput().isKeyPressed(Input.KEY_NUMPAD1)) {
+			return lastpressedp1="One";
+		}
+		return "";
+		
+	}
+	
+	public String getPressedpl2(GameContainer gc)
+	{
+
+		if(gc.getInput().isKeyPressed(Input.KEY_UP)){
+			return lastpressedp1="Up";
+			}
+		if(gc.getInput().isKeyPressed(Input.KEY_DOWN)){
+			return lastpressedp1="Down";
+			}
+		if(gc.getInput().isKeyPressed(Input.KEY_LEFT)){
+			return lastpressedp1="Left";
+			}
+		if(gc.getInput().isKeyPressed(Input.KEY_RIGHT)){
+			return lastpressedp1="Right";
+			}
+		if(gc.getInput().isKeyPressed(Input.KEY_NUMPAD1)) {
+			return lastpressedp1="One";
+		}
 		return "";
 		
 	}
