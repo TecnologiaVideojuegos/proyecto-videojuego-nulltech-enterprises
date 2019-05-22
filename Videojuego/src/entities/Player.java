39a09a378f89f9646b169ac17073dfe45fdace8c
yosua.martinez <yosua@mifor.es>
2019-05-22 02:28:57 +0200

    Fix

--- Videojuego/src/entities/Player.java
+++ Videojuego/src/entities/Player.java
@@ -15,7 +15,7 @@ public class Player {
 	 */
 	private final String name;
 	private int puntuation;
-	private int availableMovements = 100;
+	private int availableMovements = 1;
 	
 	private Coordinates coordinates;
 	private MapLocation mapLocation;
